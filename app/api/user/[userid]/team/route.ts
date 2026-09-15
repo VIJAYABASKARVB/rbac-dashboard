2962371fa986e36ead10de6498eb29e10e3273ba
Vijaya Baskar P <vbluben@gmail.com>
2026-09-15 15:24:17 +0530

    api:assign and remove users to team

---
+++ app/api/user/[userid]/team/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from "next/server";
+import { checkUserPermission, getCurrentUser } from "../../../../lib/auth";
+import { Role } from "@prisma/client";
+import { prisma } from "../../../../lib/db";
+
+export async function PATCH(
+  request:NextRequest,
+  context:{params:Promise<{userId:string}>}) {
+  
+  try{
+    const {userId} = await context.params;
+    const user = await getCurrentUser();
+
+    if(!user || !checkUserPermission(user,Role.ADMIN)){
+      return NextResponse.json(
+        {error:"You are not authorized to assign role"},
+        {status:401}
+      );
+    }
+    const teamId = await request.json();
+
+    if (teamId) {
+      const team = await prisma.team.findUnique({
+        where: { id:teamId },
+      });
+
+      if (!team) {
+        return NextResponse.json(
+          {
+            error: "Team not found"
+          },
+          { status: 404 }
+        )
+      }
+    }
+
+    // updation
+    const updatedUser = await prisma.user.update({
+      where:{id:userId},
+      data:{
+        teamId:teamId,
+      },
+      include:{
+        team:true,
+      },
+    });
+
+    return NextResponse.json({
+      user:updatedUser,
+      message:teamId
+        ? "User assigned to team successfully" 
+        : "User removed from team successfully",
+    })
+  }catch(error){
+    console.error("Team assignment error:",error);
+    if(error instanceof Error && 
+      error.message.includes("Record to update not found")){
+        return NextResponse.json(
+          {error:"User not found"},
+          {status:404}
+        )
+    }
+    return NextResponse.json(
+      {error:"Internal server error,something went wrong!"},
+      {status:500}
+    )
+  }
+}
